6243ddafd386e79f5c13c5dc559458f0aa41ed14
hochmoon <s01099760587@gmail.com>
2023-05-13 16:37:50 +0900

    2023/05/13 // 심규영 // book1 ch02 완료

---
+++ Book1/Ch02/연습문제/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 연습문제
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?\n반갑습니다!");
+        }
+    }
+}
